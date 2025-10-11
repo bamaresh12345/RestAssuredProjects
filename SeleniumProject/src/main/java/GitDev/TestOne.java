287143c34635cf4edc4bf6766014d1c9572d5349
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-10-11 12:26:16 -0400

    commit all files (#7)

--- SeleniumProject/src/main/java/GitDev/TestOne.java
+++ SeleniumProject/src/main/java/GitDev/TestOne.java
@@ -6,5 +6,6 @@ public class TestOne {
     public static void main(String[] args) {
 
         System.out.println("TestOne added File");
+        System.out.println("check local or Remote");
     }
 }
