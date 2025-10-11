471f21da1e8e286c64cb56a0e39b4bf0955299be
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-10-11 12:30:22 -0400

    commit all files (#8)

--- SeleniumProject/src/test/java/GitTesting1/TestOne1.java
+++ SeleniumProject/src/test/java/GitTesting1/TestOne1.java
@@ -6,6 +6,10 @@ public class TestOne1 {
     public static void main(String[] args) {
 
         System.out.println("TestOne1 File");
+
+        System.out.println("Local Main");
+
         System.out.println("check local or Remote");
+
     }
 }
