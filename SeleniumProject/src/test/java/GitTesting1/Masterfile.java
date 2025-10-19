6ec7e388d02725d165c4842e11ad329a8251f3be
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-10-18 20:31:13 -0400

    Fix duplicate code in Masterfile.java
    Removed duplicate variable declarations and print statements.

--- SeleniumProject/src/test/java/GitTesting1/Masterfile.java
+++ SeleniumProject/src/test/java/GitTesting1/Masterfile.java
@@ -8,5 +8,12 @@ public class Masterfile {
         int sum = num1 + num2;  // add the numbers
 
         System.out.println("The sum is: " + sum);
+
+          int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int sum = num1 + num2;  // add the numbers
+
+        System.out.println("The sum is: " + sum);
     }
 }
