a9345501f91f0cccc570d2cd985b20bb1c6f699e
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-10-18 20:16:25 -0400

    Add Masterfile.java for adding two numbers

---
+++ SeleniumProject/src/test/java/GitTesting1/Masterfile.java
@@ -0,0 +1,12 @@
+this for teistng from MAstre branch file newly added
+// Simple program to add two numbers in Java
+public class Masterfile {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int sum = num1 + num2;  // add the numbers
+
+        System.out.println("The sum is: " + sum);
+    }
+}
