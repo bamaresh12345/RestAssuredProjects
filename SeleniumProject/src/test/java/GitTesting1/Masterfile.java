03ca6a61157c1eecfef9e8f9dc11188bebeca502
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-10-18 20:41:16 -0400

    Accounts (#19)
    * Account1Dev  Commit
    
    * Account1Dev  Commit
    
    * Update Accounts4.java
    
    * Account1Dev  Commit
    
    * Update Accounts4.java
    
    * Account1Dev  Commit
    
    * Update Account3.java
    
    * Update Account1Dev.java
    
    * Account1Dev  Commit
    
    * Account1Dev  Commit
    
    * Account1Dev  Commit
    
    * Account1Dev  Commit
    
    * Account1Dev  Commit

--- SeleniumProject/src/test/java/GitTesting1/Masterfile.java
+++ SeleniumProject/src/test/java/GitTesting1/Masterfile.java
@@ -9,10 +9,10 @@ public class Masterfile {
 
         System.out.println("The sum is: " + sum);
 
-          int num1 = 10;  // first number
-        int num2 = 20;  // second number
+           num1 = 10;  // first number
+         num2 = 20;  // second number
 
-        int sum = num1 + num2;  // add the numbers
+         sum = num1 + num2;  // add the numbers
 
         System.out.println("The sum is: " + sum);
     }
