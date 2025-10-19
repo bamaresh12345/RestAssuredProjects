89cd29e658038e8189d61f1bc1b45511e174fc18
bamaresh12345 <bamaresh2012@gmail.com>
2025-10-18 20:39:33 -0400

    Account1Dev  Commit

--- SeleniumProject/src/test/java/GitTesting1/Masterfile.java
+++ SeleniumProject/src/test/java/GitTesting1/Masterfile.java
@@ -1,4 +1,6 @@
-this for teistng from MAstre branch file newly added
+package GitTesting1;
+
+//this for teistng from MAstre branch file newly added
 // Simple program to add two numbers in Java test
 public class Masterfile {
     public static void main(String[] args) {
@@ -9,10 +11,10 @@ public class Masterfile {
 
         System.out.println("The sum is: " + sum);
 
-          int num1 = 10;  // first number
-        int num2 = 20;  // second number
+           num1 = 10;  // first number
+         num2 = 20;  // second number
 
-        int sum = num1 + num2;  // add the numbers
+         sum = num1 + num2;  // add the numbers
 
         System.out.println("The sum is: " + sum);
     }
